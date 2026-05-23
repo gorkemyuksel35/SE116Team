5d804329450f12818f60b547b82af4e3472bf419
gorkemyuksel35 <gorkem.yuksel@std.ieu.edu.tr>
2026-05-23 20:32:29 +0300

    I wrote InternetHub.java, PowerPlant.java, UtilityProvider.java and WaterStation.java, and I tested them on my Main.java after that I saw they are working clearly.

--- cells/UtilityProvider.java
+++ cells/UtilityProvider.java
@@ -1,4 +1,25 @@
 package cells;
 
-public class UtilityProvider {
+public abstract class UtilityProvider extends Cell {
+    protected int cap;
+    protected String utilType;
+
+    public UtilityProvider(int x, int y, char symbol, String utilType) {
+        super(x, y, symbol);
+        this.cap = 100;
+        this.utilType = utilType;
+    }
+
+    @Override
+    public boolean isConnectable() {
+        return true;
+    }
+
+    public int getCap() {
+        return cap;
+    }
+
+    public String getUtilType() {
+        return utilType;
+    }
 }
--- core/Main.java
+++ core/Main.java
@@ -8,13 +8,18 @@
 
 package core;
 
+import cells.InternetHub;
+import cells.PowerPlant;
 import cells.Road;
+import cells.WaterStation;
 
 public class Main {
     public static void main(String[] args) {
         CityMap testCM = new CityMap(5,5);
         testCM.setCell(1, 1, new Road(1, 1));
-        testCM.setCell(2, 1, new Road(2, 1));
+        testCM.setCell(1, 2, new PowerPlant(1, 2));
+        testCM.setCell(1, 3, new WaterStation(1, 3));
+        testCM.setCell(1, 4, new InternetHub(1, 4));
         testCM.printMap();
     }
 }
